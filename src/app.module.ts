64ad075bd3ff8b9566bd2037043f45e81840e424
Moisés Freitas <moisesfreitas580@gmail.com>
2026-06-22 18:56:00 -0300

    Correção

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosModule } from './produtos/produtos.module';
 import { ImagesModule } from './images/images.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProdutosModule, ImagesModule],
+  imports: [ProdutosModule, ImagesModule, ConfigModule.forRoot({ isGlobal: true }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
