607ff6c5d7a0887a7e6cc40bc16257d3931123e6
Moisés Freitas <moisesfreitas580@gmail.com>
2026-07-14 20:16:00 -0300

    Teste

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `${process.env.AWS_S3_BUCKET_NAME as string} ${process.env.AWS_ENDPOINT_URL as string} ${process.env.AWS_DEFAULT_REGION as string}`;
   }
 }
